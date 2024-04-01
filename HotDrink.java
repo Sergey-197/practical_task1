051379da1affe863378f7333e6b4718977081820
Sergey <spumen35@gmail.com>
2024-04-01 12:12:43 +0300

    .\HotDrink.java

---
+++ HotDrink.java
@@ -0,0 +1,11 @@
+class HotDrink {
+    String name;
+    int volume;
+    int temperature;
+
+    public HotDrink(String name, int volume, int temperature) {
+        this.name = name;
+        this.volume = volume;
+        this.temperature = temperature;
+    }
+}
